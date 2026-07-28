1603a67f5ddb391347f3ed4e9c81b8cf0f19731f
Jeevasruthi-20 <jeevasruthi20@gmail.com>
2026-07-28 20:56:26 +0530

    deploy for firebase

--- doc-app-main/frontend/src/firebaseConfig.js
+++ doc-app-main/frontend/src/firebaseConfig.js
@@ -4,13 +4,10 @@ import { getAuth, GoogleAuthProvider, FacebookAuthProvider, OAuthProvider } from
 
 // Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyBdnM0qq97wUCbh0Rs_11X98SihX_-e6UQ",
-  authDomain: "doctor-appointment-app-ba87e.firebaseapp.com",
-  projectId: "doctor-appointment-app-ba87e",
-  storageBucket: "doctor-appointment-app-ba87e.appspot.com",
-  messagingSenderId: "718938056522",
-  appId: "1:718938056522:web:06165786995a17b924c3b3",
-  measurementId: "G-ZGPVZWY99D",
+  apiKey: "AIzaSyBc9LghupbtHaEVMZwhimKAAGYQmoEt4lI",
+  authDomain: "medtech-f8614.firebaseapp.com",
+  projectId: "medtech-f8614",
+  storageBucket: "medtech-f8614.appspot.com",
 };
 
 // Initialize Firebase
